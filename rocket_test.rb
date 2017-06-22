a1039d334b919fea0005aa672c7e54c8c13597b6
Roopesh <rrattan.89@gmail.com>
2017-06-22 18:16:42 -0400

    tests for lift off

--- rocket_test.rb
+++ rocket_test.rb
@@ -27,4 +27,30 @@ class RocketTest < Minitest::Test
     refute @rocket.colour == "orange"
   end
 
+  def test_lift_off_with_rocket_grounded
+    @rocket.lift_off == true
+    @rocket.flying? == true
+  end
+
+  def test_lift_off_with_rocket_in_flight
+    @rocket.lift_off
+    @rocket.lift_off == false
+    @rocket.flying? == true
+  end
+
+  def test_land_with_rocket_in_flight
+  end
+
+  def test_grounded_rocket_cant_land
+  end
+
+  def test_flying_rocket_not_flying_after_landing
+  end
+
+  def test_status_is_flying
+  end
+
+  def test_status_is_not_flying
+  end
+
 end
