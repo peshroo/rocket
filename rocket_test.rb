38a65893df168748a12fe11113818b57a2239817
Roopesh <rrattan.89@gmail.com>
2017-06-22 17:50:27 -0400

    setup and test for name + diff name

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,18 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_new_name
+    @rocket.name = ("Bob")
+    assert @rocket.name = "Bob"
+  end
+
+  def test_different_new_name
+    @rocket.name = ("Bob")
+    refute @rocket.name == "Hello"
+  end
+
 end
